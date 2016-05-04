defaffb08cc96a2487f357ac06d44722d05c1289
Jonathan Wilson Bruce <jwbmw3@gmail.com>
2016-05-04 16:52:15 -0400

    liftoff on 05/04/16

--- client/main.js
+++ client/main.js
@@ -4,6 +4,16 @@ if(Meteor.isClient){
   Template.wishlist.helpers({
     'wishlist' = function(){
       return wishlist.find()
+    },
+    'selectedWish': function(){
+      // code goes here
+    }
+  });
+  Template.wishlist.events({
+    'click .player' = function(){
+      var playerId = this._id;
+      console.log("You clicked a .player element");
+      Session.set('selectedWish', playerId);
     }
   });
 }
