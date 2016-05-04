ffa2a675d395ae8fcbff5578d114b64dddc6bf32
Jonathan Wilson Bruce <jwbmw3@gmail.com>
2016-05-03 22:21:46 -0400

    Liftoff on 05/03/16

--- client/main.js
+++ client/main.js
@@ -1,1 +1,11 @@
 Wishlist = new Mongo.collection('wishlist');
+console.log("Hello World");
+if(Meteor.isClient){
+  Template.wishlist.helpers({
+    'wishlist' = function(){
+      return wishlist.find()
+    }
+  });
+}
+if(Meteor.isServer){
+}
